59f7783b5ce7c2696ad845983248616390323e12
Wellington Souza Abreu <wellingtonsouza6300@gmail.com>
2022-11-21 09:47:41 -0400

    feat(structure): Implements socialImpactCategories on social impact flow

--- app/src/screens/socialImpactScreens/socialImpactCategories.ts
+++ app/src/screens/socialImpactScreens/socialImpactCategories.ts
@@ -1,1 +1,79 @@
-export const socialImpactCategories = {}
+import { SocialImpactCategories } from './../../services/Firebase/types'
+
+export const updateSocialImpactTags = (category: SocialImpactCategories, tag: string) => {
+    socialImpactCategories[category].tags = [...socialImpactCategories[category].tags, tag]
+}
+
+const commonTags = [
+    'fome',
+    'roupas',
+    'educação',
+    'renda e trabalho',
+    'paz e justiça',
+    'igualdade social',
+    'moradia',
+    'igualdade de gênero',
+    'energia',
+    'água',
+    'mudança climática',
+    'biodiversidade',
+    'polução',
+    'preservação ',
+]
+
+let socialImpactCategories = {
+    ONGs: {
+        label: 'ONGs',
+        value: 'ONGs',
+        tags: commonTags
+    },
+
+    popularIniciatives: {
+        label: 'iniciativas populares',
+        value: 'popularIniciatives',
+        tags: commonTags
+    },
+
+    charityBazaars: {
+        label: 'bazares beneficentes',
+        value: 'charityBazaars',
+        tags: commonTags
+    },
+
+    associations: {
+        label: 'associações',
+        value: 'associations',
+        tags: commonTags
+    },
+    collective: {
+        label: 'coletivos',
+        value: 'collective',
+        tags: commonTags
+    },
+
+    socialProjects: {
+        label: 'projetos sociais',
+        value: 'socialProjects',
+        tags: commonTags
+    },
+
+    volounteering: {
+        label: 'voluntariado',
+        value: 'volounteering',
+        tags: commonTags
+    },
+
+    donations: {
+        label: 'doações',
+        value: 'donations',
+        tags: commonTags
+    },
+
+    others: {
+        label: 'outros',
+        value: 'others',
+        tags: []
+    },
+}
+
+export { socialImpactCategories }
