502a2cf7598383d546b5fbcf1e5123cd9dbd18a7
Wellington Souza Abreu <wellingtonsouza6300@gmail.com>
2023-01-11 12:15:32 -0400

    feat(screens): Adapt DefaultHeaderContainer to ios devices

--- app/src/components/_containers/DefaultHeaderContainer/styles.ts
+++ app/src/components/_containers/DefaultHeaderContainer/styles.ts
@@ -1,8 +1,10 @@
 import styled from 'styled-components/native'
-import { Animated } from 'react-native'
+import Constants from 'expo-constants'
+import { Animated, Platform } from 'react-native'
 import { RFValue } from 'react-native-responsive-fontsize'
 
 export const Container = styled(Animated.View)`
+	padding-top:${Platform.OS === 'ios' ? Constants.statusBarHeight : 0}px;
     border-bottom-width: ${RFValue(5)}px;
     border-bottom-color:  ${({ theme }) => theme.black4};
     transition: background-color 1s ease;
