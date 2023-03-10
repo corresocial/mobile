5bf06d7cfd0816f2e8ce303a305593bac8494b2f
JpMCarrilho <jpmcarrilho@gmail.com>
2023-03-10 17:16:36 -0300

    fix: location permission homefeed

--- app/src/screens/homeScreens/Home/index.tsx
+++ app/src/screens/homeScreens/Home/index.tsx
@@ -9,7 +9,7 @@ import {
 	FooterSigh,
 	HorizontalPostTypes,
 	RecentPostsContainer,
-	Sigh
+	Sigh,
 } from './styles'
 import { theme } from '../../../common/theme'
 import SocialImpactIcon from '../../../assets/icons/socialImpact-filled.svg'
@@ -18,7 +18,10 @@ import CultureIcon from '../../../assets/icons/culture-filled.svg'
 import ServiceIcon from '../../../assets/icons/service-filled.svg'
 import VacancyIcon from '../../../assets/icons/vacancy-filled.svg'
 
-import { relativeScreenHeight, relativeScreenWidth } from '../../../common/screenDimensions'
+import {
+	relativeScreenHeight,
+	relativeScreenWidth,
+} from '../../../common/screenDimensions'
 
 import { generateGeohashes } from '../../../common/generateGeohashes'
 import { searchAddressByText } from '../../../services/maps/searchAddressByText'
@@ -27,7 +30,12 @@ import { getRecentAddressFromStorage } from '../../../services/maps/recentAddres
 import { getPostsByLocationCloud } from '../../../services/cloudFunctions/getPostsByLocationCloud'
 // import { getPostsByLocation } from '../../../services/firebase/post/getPostsByLocation'
 
-import { SearchParams, LatLong, AddressSearchResult, SelectedAddressRender } from '../../../services/maps/types'
+import {
+	SearchParams,
+	LatLong,
+	AddressSearchResult,
+	SelectedAddressRender,
+} from '../../../services/maps/types'
 import { Id, PostCollection, PostType } from '../../../services/firebase/types'
 import { HomeScreenProps } from '../../../routes/Stack/HomeStack/stackScreenProps'
 
@@ -45,18 +53,22 @@ import { FocusAwareStatusBar } from '../../../components/FocusAwareStatusBar'
 
 const initialSelectedAddress = {
 	addressHighlighted: '',
-	addressThin: ''
+	addressThin: '',
 }
 
 function Home({ navigation }: HomeScreenProps) {
 	const { userDataContext } = useContext(AuthContext)
 	const { setLoaderIsVisible } = useContext(LoaderContext)
-	const { locationDataContext, setLocationDataOnContext } = useContext(LocationContext)
+	const { locationDataContext, setLocationDataOnContext } =		useContext(LocationContext)
 
-	const [selectedAddress, setSelectedAddress] = useState<SelectedAddressRender>(initialSelectedAddress)
-	const [recentAddresses, setRecentAddresses] = useState<AddressSearchResult[]>([])
+	const [selectedAddress, setSelectedAddress] =		useState<SelectedAddressRender>(initialSelectedAddress)
+	const [recentAddresses, setRecentAddresses] = useState<
+		AddressSearchResult[]
+	>([])
 	const [nearPosts, setNearPosts] = useState<PostCollection[]>([])
-	const [addressSuggestions, setAddressSuggestions] = useState<AddressSearchResult[]>([])
+	const [addressSuggestions, setAddressSuggestions] = useState<
+		AddressSearchResult[]
+	>([])
 	const [hasLocationPermission, setHasLocationPermission] = useState(false)
 	const [hasLocationEnable, setHasLocationEnable] = useState(false)
 	const [searchEnded, setSearchEnded] = useState(false)
@@ -86,16 +98,18 @@ function Home({ navigation }: HomeScreenProps) {
 		getRecentAddresses()
 	}, [])
 
-	useEffect(() => {
-		findNearPosts('', true)
-	}, [hasLocationPermission])
-
 	const requestPermissions = async () => {
 		const { status } = await Location.requestForegroundPermissionsAsync()
 		if (status === 'granted') {
 			setHasLocationPermission(true)
 		}
 	}
+
+	useEffect(() => {
+		if (hasLocationPermission) {
+			findNearPosts('', true)
+		}
+	}, [hasLocationPermission])
 
 	const locationIsEnable = async () => {
 		const locationEnabled = await Location.hasServicesEnabledAsync()
@@ -108,27 +122,42 @@ function Home({ navigation }: HomeScreenProps) {
 		setRecentAddresses(addresses)
 	}
 
-	const findNearPosts = async (searchText: string, currentPosition?: boolean, alternativeCoordinates?: LatLong, refresh?: boolean) => {
+	const findNearPosts = async (
+		searchText: string,
+		currentPosition?: boolean,
+		alternativeCoordinates?: LatLong,
+		refresh?: boolean
+	) => {
 		if (!locationIsEnable) return
 
 		try {
-			refresh ? setFlatListIsLoading(true) : setLoaderIsVisible(true); setSearchEnded(false)
+			refresh ? setFlatListIsLoading(true) : setLoaderIsVisible(true)
+			setSearchEnded(false)
 
 			let searchParams = {} as SearchParams
 			if (currentPosition) {
 				const coordinates = await getCurrentPositionCoordinates()
 				searchParams = await getSearchParams(coordinates)
 			} else {
-				const coordinates = alternativeCoordinates || await getSearchedAddressCoordinates(searchText)
+				const coordinates =					alternativeCoordinates
+					|| (await getSearchedAddressCoordinates(searchText))
 				searchParams = await getSearchParams(coordinates as LatLong) // address converter
 			}
 
-			const nearbyPosts = await getPostsByLocationCloud(searchParams, userDataContext.userId as Id)
+			const nearbyPosts = await getPostsByLocationCloud(
+				searchParams,
+				userDataContext.userId as Id
+			)
 			// const nearbyPosts = await getPostsByLocation(searchParams)
 			setNearPosts(nearbyPosts || [])
 
-			refresh ? setFlatListIsLoading(false) : setLoaderIsVisible(false); setSearchEnded(true)
-			setLocationDataOnContext({ searchParams, nearbyPosts, lastRefreshInMilliseconds: Date.now() })
+			refresh ? setFlatListIsLoading(false) : setLoaderIsVisible(false)
+			setSearchEnded(true)
+			setLocationDataOnContext({
+				searchParams,
+				nearbyPosts,
+				lastRefreshInMilliseconds: Date.now(),
+			})
 		} catch (err) {
 			console.log(err)
 			setLoaderIsVisible(false)
@@ -137,15 +166,20 @@ function Home({ navigation }: HomeScreenProps) {
 	}
 
 	const refreshFlatlist = async () => {
-		await findNearPosts('', false, locationDataContext.searchParams.coordinates || null, true)
+		await findNearPosts(
+			'',
+			false,
+			locationDataContext.searchParams.coordinates || null,
+			true
+		)
 	}
 
 	const getCurrentPositionCoordinates = async () => {
-		const currentPositionCoordinate = await Location.getCurrentPositionAsync()
+		const currentPositionCoordinate =			await Location.getCurrentPositionAsync()
 
 		return {
 			lat: currentPositionCoordinate.coords.latitude,
-			lon: currentPositionCoordinate.coords.longitude
+			lon: currentPositionCoordinate.coords.longitude,
 		} as LatLong
 	}
 
@@ -162,13 +196,19 @@ function Home({ navigation }: HomeScreenProps) {
 	}
 
 	const getSearchParams = async (coordinates: LatLong) => {
-		const address = await convertGeocodeToAddress(coordinates.lat, coordinates.lon)
+		const address = await convertGeocodeToAddress(
+			coordinates.lat,
+			coordinates.lon
+		)
 		const structuredAddress = structureAddress(address)
-		const geohashObject = generateGeohashes(coordinates.lat, coordinates.lon)
+		const geohashObject = generateGeohashes(
+			coordinates.lat,
+			coordinates.lon
+		)
 
 		setSelectedAddress({
 			addressHighlighted: `${structuredAddress.street}, ${structuredAddress.number} - ${structuredAddress.district}`,
-			addressThin: `${structuredAddress.city} - ${structuredAddress.state}, ${structuredAddress.postalCode}`
+			addressThin: `${structuredAddress.city} - ${structuredAddress.state}, ${structuredAddress.postalCode}`,
 		})
 
 		return {
@@ -178,14 +218,16 @@ function Home({ navigation }: HomeScreenProps) {
 			postType: 'service',
 			coordinates,
 			geohashes: geohashObject.geohashNearby,
-
 		} as SearchParams
 	}
 
-	const convertGeocodeToAddress = async (latitude: number, longitude: number) => {
+	const convertGeocodeToAddress = async (
+		latitude: number,
+		longitude: number
+	) => {
 		const geocodeAddress = await Location.reverseGeocodeAsync({
 			latitude,
-			longitude
+			longitude,
 		})
 		return geocodeAddress
 	}
@@ -206,33 +248,49 @@ function Home({ navigation }: HomeScreenProps) {
 	}
 
 	const saveRecentAddresses = (newAddress: AddressSearchResult) => {
-		const filtredRecentAddress = recentAddresses.filter((address) => address.formattedAddress !== newAddress.formattedAddress)
-		setRecentAddresses([{ ...newAddress, recent: true }, ...filtredRecentAddress])
+		const filtredRecentAddress = recentAddresses.filter(
+			(address) => address.formattedAddress !== newAddress.formattedAddress
+		)
+		setRecentAddresses([
+			{ ...newAddress, recent: true },
+			...filtredRecentAddress,
+		])
 	}
 
 	const goToPostView = (item: PostCollection) => {
 		switch (item.postType) {
 			case 'service': {
-				navigation.navigate('ViewServicePostHome', { postData: { ...item } })
+				navigation.navigate('ViewServicePostHome', {
+					postData: { ...item },
+				})
 				break
 			}
 			case 'sale': {
-				navigation.navigate('ViewSalePostHome', { postData: { ...item } })
+				navigation.navigate('ViewSalePostHome', {
+					postData: { ...item },
+				})
 				break
 			}
 			case 'vacancy': {
-				navigation.navigate('ViewVacancyPostHome', { postData: { ...item } })
+				navigation.navigate('ViewVacancyPostHome', {
+					postData: { ...item },
+				})
 				break
 			}
 			case 'socialImpact': {
-				navigation.navigate('ViewSocialImpactPostHome', { postData: { ...item } })
+				navigation.navigate('ViewSocialImpactPostHome', {
+					postData: { ...item },
+				})
 				break
 			}
 			case 'culture': {
-				navigation.navigate('ViewCulturePostHome', { postData: { ...item } })
+				navigation.navigate('ViewCulturePostHome', {
+					postData: { ...item },
+				})
 				break
 			}
-			default: return false
+			default:
+				return false
 		}
 	}
 
@@ -242,7 +300,7 @@ function Home({ navigation }: HomeScreenProps) {
 
 	const navigateToProfile = (userId: string) => {
 		if (userDataContext.userId === userId) {
-			navigation.navigate('Profile' as any)// TODO Type
+			navigation.navigate('Profile' as any) // TODO Type
 			return
 		}
 		navigation.navigate('ProfileHome', { userId })
@@ -250,7 +308,10 @@ function Home({ navigation }: HomeScreenProps) {
 
 	return (
 		<Container>
-			<FocusAwareStatusBar backgroundColor={theme.orange2} barStyle={'dark-content'} />
+			<FocusAwareStatusBar
+				backgroundColor={theme.orange2}
+				barStyle={'dark-content'}
+			/>
 			<DropdownContainer>
 				<LocationNearDropdown
 					selectedAddress={selectedAddress}
@@ -324,55 +385,68 @@ function Home({ navigation }: HomeScreenProps) {
 				<SubtitleCard
 					text={'posts de recentes'}
 					highlightedText={['recentes']}
-					onPress={() => { }}
+					onPress={() => {}}
 				/>
-				{
-					(!hasLocationEnable && !nearPosts.length) && (
-						<RequestLocation getLocationPermissions={() => {
+				{!hasLocationEnable && !nearPosts.length && (
+					<RequestLocation
+						getLocationPermissions={() => {
 							requestPermissions()
 							findNearPosts('', true)
 						}}
+					/>
+				)}
+				{nearPosts.length ? (
+					<FlatList
+						data={nearPosts}
+						renderItem={({ item }) => (
+							<PostCard
+								post={item}
+								owner={item.owner}
+								navigateToProfile={navigateToProfile}
+								onPress={() => goToPostView(item)}
+							/>
+						)}
+						// onEndReached={() => console.log('findMoreNearPosts(Pagination)')}
+						showsVerticalScrollIndicator={false}
+						contentContainerStyle={{
+							padding: RFValue(10),
+							paddingTop: flatListIsLoading
+								? relativeScreenHeight(10)
+								: RFValue(10),
+						}}
+						ItemSeparatorComponent={() => <Sigh />}
+						ListHeaderComponentStyle={{ marginBottom: RFValue(15) }}
+						ListFooterComponent={<FooterSigh />}
+						refreshControl={(
+							<RefreshControl
+								colors={[
+									theme.orange3,
+									theme.pink3,
+									theme.green3,
+									theme.blue3,
+									theme.purple3,
+									theme.yellow3,
+									theme.red3,
+								]}
+								refreshing={flatListIsLoading}
+								progressBackgroundColor={theme.white3}
+								onRefresh={refreshFlatlist}
+							/>
+						)}
+					/>
+				) : (
+					hasLocationEnable
+					&& searchEnded && (
+						<WithoutPostsMessage
+							title={'opa!'}
+							message={
+								'parece que não temos nenhum post perto de você, nosso time já está sabendo e irá resolver!'
+							}
 						/>
 					)
-				}
-				{
-					nearPosts.length
-						? (
-							<FlatList
-								data={nearPosts}
-								renderItem={({ item }) => (
-									<PostCard
-										post={item}
-										owner={item.owner}
-										navigateToProfile={navigateToProfile}
-										onPress={() => goToPostView(item)}
-									/>
-								)}
-								// onEndReached={() => console.log('findMoreNearPosts(Pagination)')}
-								showsVerticalScrollIndicator={false}
-								contentContainerStyle={{ padding: RFValue(10), paddingTop: flatListIsLoading ? relativeScreenHeight(10) : RFValue(10) }}
-								ItemSeparatorComponent={() => <Sigh />}
-								ListHeaderComponentStyle={{ marginBottom: RFValue(15) }}
-								ListFooterComponent={<FooterSigh />}
-								refreshControl={(
-									<RefreshControl
-										colors={[theme.orange3, theme.pink3, theme.green3, theme.blue3, theme.purple3, theme.yellow3, theme.red3]}
-										refreshing={flatListIsLoading}
-										progressBackgroundColor={theme.white3}
-										onRefresh={refreshFlatlist}
-									/>
-								)}
-							/>
-						)
-						: hasLocationEnable && searchEnded && (
-							<WithoutPostsMessage
-								title={'opa!'}
-								message={'parece que não temos nenhum post perto de você, nosso time já está sabendo e irá resolver!'}
-							/>
-						)
-				}
+				)}
 			</RecentPostsContainer>
-		</Container >
+		</Container>
 	)
 }
 
