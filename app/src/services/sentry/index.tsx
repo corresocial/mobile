54cb635f0a64b565c0eea53dd7183dd6b2434931
Wellington Souza Abreu <wellingtonsouza6300@gmail.com>
2023-03-29 10:00:19 -0400

    chore: Modify sentry config to development

--- app/src/services/sentry/index.tsx
+++ app/src/services/sentry/index.tsx
@@ -3,8 +3,8 @@ import { DSN_SENTRY } from '@env'
 const sentryConfig = {
 	dsn: DSN_SENTRY,
 	enableInExpoDevelopment: true,
-	debug: false, // Set it to `FALSE` in production
-	enableNative: true, // Set it to `TRUE` in production
+	debug: true, // Set it to `FALSE` in production
+	enableNative: false, // Set it to `TRUE` in production
 	autoInitializeNativeSdk: true
 }
 
