d156c27a548e528dca4954620f2cfb3157a177ea
Wellington Souza Abreu <wellingtonsouza6300@gmail.com>
2023-03-30 15:20:01 -0400

    chore(chat): Expand chat context

--- app/src/routes/Stack/ChatStack/index.tsx
+++ app/src/routes/Stack/ChatStack/index.tsx
@@ -4,27 +4,22 @@ import { createStackNavigator, TransitionPresets } from '@react-navigation/stack
 
 import { ChatStackParamList } from './types'
 
-import { ChatProvider } from '../../../contexts/ChatContext'
-
 import { ChatConversations } from '../../../screens/chatScreens/ChatConversations'
-import { ChatMessages } from '../../../screens/chatScreens/ChatMessages'
 
 const Stack = createStackNavigator<ChatStackParamList>()
 
 export function ChatStack() {
 	return (
-		<ChatProvider>
-			<Stack.Navigator
-				initialRouteName={'ChatConversations'}
-				screenOptions={{
-					headerShown: false,
-					gestureEnabled: true,
-					...TransitionPresets.SlideFromRightIOS,
-				}}
-			>
-				<Stack.Screen name={'ChatConversations'} component={ChatConversations} />
-				{/* <Stack.Screen name={'ChatMessages'} component={ChatMessages} /> */}
-			</Stack.Navigator>
-		</ChatProvider>
+		<Stack.Navigator
+			initialRouteName={'ChatConversations'}
+			screenOptions={{
+				headerShown: false,
+				gestureEnabled: true,
+				...TransitionPresets.SlideFromRightIOS,
+			}}
+		>
+			<Stack.Screen name={'ChatConversations'} component={ChatConversations} />
+			{/* <Stack.Screen name={'ChatMessages'} component={ChatMessages} /> */}
+		</Stack.Navigator>
 	)
 }
