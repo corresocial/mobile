522323e7af5b2d332e0108fb5efc57ca3d340705
Wellington Souza Abreu <wellingtonsouza6300@gmail.com>
2023-05-09 19:01:57 -0400

    feat(context): Implements treatment to simplify post on vacancy, culture e impact contexts

--- app/src/contexts/SocialImpactContext.tsx
+++ app/src/contexts/SocialImpactContext.tsx
@@ -1,10 +1,14 @@
-import React, { createContext, useMemo, useState } from 'react'
+import React, { createContext, useContext, useMemo, useState } from 'react'
 
 import { SocialImpactData } from './types'
+import { AuthContext } from './AuthContext'
+import { SocialImpactCollectionRemote } from '../services/firebase/types'
 
 type SocialImpactContextType = {
+	isSecondPost: boolean,
 	socialImpactDataContext: SocialImpactData
 	setSocialImpactDataOnContext: (data: SocialImpactData) => void
+	getAditionalDataFromLastPost: () => void
 }
 
 interface SocialImpactProviderProps {
@@ -12,24 +16,51 @@ interface SocialImpactProviderProps {
 }
 
 const initialValue = {
+	isSecondPost: false,
 	socialImpactDataContext: {},
-	setSocialImpactDataOnContext: (data: SocialImpactData) => { }
+	setSocialImpactDataOnContext: (data: SocialImpactData) => { },
+	getAditionalDataFromLastPost: () => { }
 }
 
 const SocialImpactContext = createContext<SocialImpactContextType>(initialValue)
 
 function SocialImpactProvider({ children }: SocialImpactProviderProps) {
+	const { userDataContext } = useContext(AuthContext)
+
+	const [isSecondPost, setIsSecondPost] = useState(false)
 	const [socialImpactDataContext, setSocialImpactDataContext] = useState(initialValue.socialImpactDataContext)
 
 	const setSocialImpactDataOnContext = async (data: SocialImpactData) => {
+		setSocialImpactDataContext({ ...socialImpactDataContext, ...data })
+	}
+
+	const getAditionalDataFromLastPost = () => {
+		const userPosts = userDataContext.posts || []
+		if (!userPosts || (userPosts && userPosts.length < 1)) return
+
+		const lastUserPost: SocialImpactCollectionRemote | any = userPosts[userPosts.length - 1] || {} // TODO Type
+		if (Object.keys(lastUserPost).length < 1) return
+
+		console.log(`Dados extraidos do post: ${lastUserPost.title}`)
+
 		setSocialImpactDataContext({
-			...socialImpactDataContext, ...data
+			range: lastUserPost.range, // EDIT
+			locationView: lastUserPost.locationView,
+			location: lastUserPost.location,
+			deliveryMethod: lastUserPost.deliveryMethod,
+			attendanceFrequency: lastUserPost.attendanceFrequency,
+			daysOfWeek: lastUserPost.daysOfWeek,
+			startHour: lastUserPost.startHour,
+			endHour: lastUserPost.endHour,
 		})
+		setIsSecondPost(true)
 	}
 
 	const socialImpactProviderData = useMemo(() => ({
+		isSecondPost,
 		socialImpactDataContext,
-		setSocialImpactDataOnContext
+		setSocialImpactDataOnContext,
+		getAditionalDataFromLastPost
 	}), [socialImpactDataContext])
 
 	return (
