ae6723b1417a8975892b88401ab83f0784935ef9
Wellington Souza Abreu <wellingtonsouza6300@gmail.com>
2023-05-18 19:02:16 -0400

    style(report): Atualiza a posição do botão de voltar na tela de denúncia

--- app/src/components/_containers/FormContainer/index.tsx
+++ app/src/components/_containers/FormContainer/index.tsx
@@ -3,9 +3,9 @@ import React, { ReactElement } from 'react'
 import { Container } from './styles'
 
 interface FormContainerProps {
-    children: ReactElement | ReactElement[]
-    backgroundColor: string,
-    justifyContent?: 'flex-start' | 'flex-end' | 'center' | 'space-between' | 'space-around' | 'space-evenly' | undefined
+	children: ReactElement | ReactElement[] | React.ReactElement<any>[] | any // TODO Type
+	backgroundColor?: string,
+	justifyContent?: 'flex-start' | 'flex-end' | 'center' | 'space-between' | 'space-around' | 'space-evenly' | undefined
 }
 
 function FormContainer({
