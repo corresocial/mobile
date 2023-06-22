0047f547b9950594f9d8c7c50bbeab16b5b24ef1
Wellington Souza <wellingtonsouza6300@gmail.com>
2023-06-22 15:41:50 -0400

    fix: Remove warns de flashlist

--- app/ignoredLogs.ts
+++ app/ignoredLogs.ts
@@ -3,5 +3,5 @@ export const ignoredLogs = [
 	`Provided value to SecureStore is larger than 2048 bytes. An attempt to store such a value will throw an error in SDK 35`,
 	`AsyncStorage has been extracted from react-native core and will be removed in a future release. It can now be installed and imported from '@react-native-async-storage/async-storage' instead of 'react-native'. See https://github.com/react-native-async-storage/async-storage`,
 	'Key "cancelled" in the image picker result is deprecated and will be removed in SDK 48, use "canceled" instead',
-	'VirtualizedLists should never be nested inside plain ScrollViews with the same orientation because it can break windowing and other functionality - use another VirtualizedList-backed container instead.'
+	`FlashList's rendered size is not usable. Either the height or width is too small (<2px). Please make sure that the parent view of the list has a valid size. FlashList will match the size of the parent.`
 ]
