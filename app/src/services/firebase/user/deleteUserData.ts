86dc00397dc1cf295e8a21d183ae679fab758de4
Wellington Souza <wellingtonsouza6300@gmail.com>
2023-08-02 16:17:17 -0400

    fix: Corrige eliminação de subcollection ao ser esquecido

--- app/src/services/firebase/user/deleteUserData.ts
+++ app/src/services/firebase/user/deleteUserData.ts
@@ -4,8 +4,10 @@ import { firestore } from '..'
 async function deleteUserData(userId: string) {
 	try {
 		const docRef = doc(firestore, 'users', userId)
+		const docPrivateRef = doc(firestore, 'users', userId, 'private', 'contacts')
 
 		await deleteDoc(docRef)
+		await deleteDoc(docPrivateRef)
 		return true
 	} catch (e) {
 		console.log(e)
