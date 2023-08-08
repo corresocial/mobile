a0361ff1fdcc0c0f9f349f0a279fea5151dfa71e
Wellington Souza <wellingtonsouza6300@gmail.com>
2023-08-08 10:06:15 -0400

    feat: Atualiza estilos e textos da tela de aceitar e continuar

--- app/src/components/CustomCarousel/styles.ts
+++ app/src/components/CustomCarousel/styles.ts
@@ -1,3 +1,4 @@
+import { RFValue } from 'react-native-responsive-fontsize'
 import styled from 'styled-components/native'
 
 export const CarouselIndicatorContainer = styled.View`
@@ -8,15 +9,15 @@ export const CarouselIndicatorContainer = styled.View`
     bottom: 30px;
     width: 100%;
 `
-export const CarouselActiveIndicatorItem = styled.View` 
-    height: 15px;
-    width: 16px;
-    border-right-width: 3px;
-    border-radius: 10px;
-    border-width: 1px;
+export const CarouselActiveIndicatorItem = styled.View`
+	height: 15px;
+	width: 16px;
+	border-right-width: ${RFValue(3)}px;
+	border-radius: ${RFValue(10)}px;
+	border-width: ${RFValue(1.5)}px;
 
     border-color: ${({ theme }) => theme.black4};
-    background-color: ${({ theme }) => theme.orange2};
+    background-color: ${({ theme }) => theme.orange3};
     margin-horizontal: 2px;
 `
 
