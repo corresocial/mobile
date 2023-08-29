f3658eed908bad5d5f93f319bc37455839c4458c
Wellington Souza <wellingtonsouza6300@gmail.com>
2023-08-29 16:48:28 -0400

    fix(vacancy): Fix style require

---
+++ app/src/screens/vacancyRegisterScreens/InsertVacancyImportantPoints/styles.ts
@@ -0,0 +1,12 @@
+import { RFValue } from 'react-native-responsive-fontsize'
+import styled from 'styled-components/native'
+
+export const Container = styled.KeyboardAvoidingView`
+    flex: 1;
+	background-color: ${({ theme }) => theme.white3};
+`
+
+export const ButtonsContainer = styled.View`
+    margin-top: ${RFValue(60)}px;
+    width: 100%;
+`
