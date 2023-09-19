8656b960cfaa21d6058568809168c9aa8afbda91
Wellington Souza <wellingtonsouza6300@gmail.com>
2023-09-19 10:33:26 -0400

    feat: Update maps env vars

--- app/app.config.js
+++ app/app.config.js
@@ -4,7 +4,18 @@ export default ({ config }) => {
 		...config,
 		android: {
 			...config.android,
-			googleServicesFile: process.env.GOOGLE_SERVICES_JSON
+			googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
+			config: {
+				googleMaps: {
+					apiKey: process.env.PROD_ANDROID_MAPS_API_KEY
+				}
+			}
+		},
+		ios: {
+			...config.ios,
+			config: {
+				googleMapsApiKey: process.env.PROD_IOS_MAPS_API_KEY
+			}
 		}
 	}
 }
