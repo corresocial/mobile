2b050b44e7e991edcfbae3140266d3351ded8325
Wellington Souza <wellingtonsouza6300@gmail.com>
2023-12-04 16:42:48 -0400

    feat: Implements path-mapping config

--- app/babel.config.js
+++ app/babel.config.js
@@ -1,7 +1,26 @@
+/* eslint-disable func-names */
 module.exports = function (api) {
 	api.cache(true)
 	return {
 		presets: ['babel-preset-expo', 'module:metro-react-native-babel-preset'],
-		plugins: ['react-native-reanimated/plugin', 'module:react-native-dotenv']
+		plugins: [
+			'react-native-reanimated/plugin',
+			'module:react-native-dotenv',
+			[
+				'module-resolver',
+				{
+					root: ['./src'],
+					alias: {
+						'@contexts': './src/contexts',
+						'@routes': './src/routes',
+						'@services': './src/services',
+						'@presentation': './src/presentation',
+						'@domain': './src/domain',
+						'@infrastructure': './src/infrastructure',
+						'@data': './src/data',
+					}
+				}
+			]
+		]
 	}
 }
