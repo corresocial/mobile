4a0c2f401b6ebcf49a9e34248dc9d19fb95818f9
Wellington Souza <wellingtonsouza6300@gmail.com>
2024-01-03 11:39:19 -0400

    fix: Warns when chat is empty

--- app/src/domain/chat/useCases/startUserChatIdsListenerUC.ts
+++ app/src/domain/chat/useCases/startUserChatIdsListenerUC.ts
@@ -1,3 +1,5 @@
+import { isArray } from 'lodash'
+
 import { Chat } from '@domain/entities/chat/types'
 import { Id } from '@domain/entities/globalTypes'
 
@@ -7,8 +9,17 @@ async function startUserChatIdsListenerUC(userId: Id, callback: (chatIds: Id[], 
 	const { existsOnDatabase, getUserChats, startUserChatIdsListener } = ChatGatewayAdapter()
 
 	const listenerCallback = async (chatIds: Id[]) => {
-		const userChats = await getUserChats(chatIds)
-		callback(chatIds, userChats)
+		let chatIdsList = chatIds
+
+		if (!isArray(chatIds)) {
+			chatIdsList = Object.values(chatIds)
+		}
+
+		const filteredChatIds = chatIdsList.filter((chatId) => chatId !== '')
+		if (!filteredChatIds || (filteredChatIds && !filteredChatIds.length)) return
+
+		const userChats = await getUserChats(filteredChatIds)
+		callback(filteredChatIds, userChats)
 	}
 
 	if (await existsOnDatabase(userId)) {
