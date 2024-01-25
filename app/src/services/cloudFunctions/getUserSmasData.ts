0e05777e4ca888936e94fc0ec253542cf4093921
Wellington Souza <wellingtonsouza6300@gmail.com>
2024-01-25 12:15:52 -0400

    feat(smas): Implementa chamada de função para a api da SMAS

---
+++ app/src/services/cloudFunctions/getUserSmasData.ts
@@ -0,0 +1,18 @@
+import axios from 'axios'
+
+import { SmasService } from '@domain/entities/smas/types'
+
+import { getEnvVars } from '@infrastructure/environment'
+
+const { FIREBASE_CLOUD_URL } = getEnvVars()
+
+const getUserSmasData = async (nis: string, smasService: SmasService) => {
+	return axios.post(`${FIREBASE_CLOUD_URL}/smasAPI`, { nis, queryType: smasService })
+		.then((res) => res.data)
+		.catch((err) => {
+			console.log(err)
+			return false
+		})
+}
+
+export { getUserSmasData }
