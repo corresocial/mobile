879dc1174fd04098031a57013dff6f99898ab9de
Luis Eduardo <luiseddlvi@gmail.com>
2024-03-13 13:16:35 -0300

    refactor(datetimepicker): refatora os componentes para adequar o novo input de datetime picker

--- app/src/presentation/components/_inputs/DataVisualizerInput/index.tsx
+++ app/src/presentation/components/_inputs/DataVisualizerInput/index.tsx
@@ -1,5 +1,7 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import DateTimePickerModal from 'react-native-modal-datetime-picker'
+
+import { getNewDate } from '@utils-ui/common/date/dateFormat'
 
 import {
 	InputContainer,
@@ -10,48 +12,75 @@ import {
 interface DataVisualizerInputProps {
 	openPickerOnTouch: boolean,
 	fields: string[],
+	initialValue?: Date,
 	pickerType?: 'date' | 'time',
 	defaultBackgroundColor: string,
 	validBackgroundColor: string,
 	onDateSelect?: (date: Date) => void
-
   }
   
 function DataVisualizerInput({ 
 	openPickerOnTouch = false,
 	fields, 
+	initialValue,
 	pickerType = 'date', 
 	defaultBackgroundColor, 
 	validBackgroundColor,
 	onDateSelect
 }: DataVisualizerInputProps) {
+	type InputValuesType = {id: number, value: string}
+	
 	const [isValid, setIsValid] = useState(false)
 	const [isDatePickerVisible, setDatePickerVisibility] = useState(false)
-	const [inputValues, setInputValues] = useState(fields.map((field, index) => ({ id: index, value: field })))
+	const [inputValues, setInputValues] = useState<InputValuesType[]>([])
+
+	useEffect(() => {
+		// console.log(!initialValue === false)
+		// if (initialValue) {
+		// 	const newDate = getNewDate(initialValue)
+		// 	const formattedValues = convertDateTimeToArrayValues(newDate)
+		// 	populateInputValues(formattedValues)
+		// 	return
+		// }
+		populateInputValues(fields)
+	}, [])
 
 	const inputPressHandler = () => {
+		console.log('oi')
 		if (openPickerOnTouch) {
+			console.log('tchau')
+
 			setDatePickerVisibility(true)
 		}
 	}
 
-	const onPickerConfirmHandler = (info: Date) => {
-		setDatePickerVisibility(false)
-		let formattedValues: number[] = []
+	const populateInputValues = (values: string[]) => {
+		const clearedValues = values.map((item, index) => ({ id: index, value: item }))
+		setInputValues(clearedValues)
+	}
+
+	const convertDateTimeToArrayValues = (dateTime: Date) => {
+		let formattedValues: string[] = []
 		switch (pickerType) {
 			case 'date': {
-				formattedValues = getFullDate(info)
+				formattedValues = getFullDate(dateTime)
 				break
 			}
 			case 'time': {
-				formattedValues = getFullTime(info)
+				formattedValues = getFullTime(dateTime)
 				break
 			}
 		} 
-		const clearedValues = inputValues.map((item, index) => ({ ...item, value: formattedValues[index].toString() }))
-		setInputValues(clearedValues)
-		onDateSelect?.(info)
+		return formattedValues
+	}
+
+	const onPickerConfirmHandler = (dateTime: Date) => {
+		const formattedValues: string[] = convertDateTimeToArrayValues(dateTime)
+		populateInputValues(formattedValues)
+
+		onDateSelect?.(dateTime)
 		setIsValid(true)
+		setDatePickerVisibility(false)
 	}
 
 	const onPickerCancelHandler = () => {
@@ -60,19 +89,19 @@ function DataVisualizerInput({
 
 	const getFullDate = (date: Date) => {
 		return [
-			date.getDate(),
-			(date.getMonth() + 1),
-			date.getFullYear()
+			date.getDate().toString(),
+			(date.getMonth() + 1).toString(),
+			date.getFullYear().toString()
 		]
 	} 
 
 	const getFullTime = (date: Date) => {
 		return [
-			date.getHours(),
-			date.getMinutes()
+			date.getHours().toString(),
+			date.getMinutes().toString()
 		]
 	}
-  
+
 	return (
 		<>
 			<InputContainer 
@@ -82,17 +111,20 @@ function DataVisualizerInput({
 				validColor={validBackgroundColor}
 				valid={isValid}
 			>
-				{inputValues.map((item) => (
-					<InputInfoSection key={item.id}>
-						<InputText>
-							{item.value} 
-						</InputText>
-					</InputInfoSection>
-				))}
+				{
+					inputValues && inputValues.map((item) => (
+						<InputInfoSection key={item.id}>
+							<InputText>
+								{item.value} 
+							</InputText>
+						</InputInfoSection>
+					))
+				}
 			</InputContainer>
 			<DateTimePickerModal
 				isVisible={isDatePickerVisible}
 				mode={pickerType}
+				date={getNewDate(initialValue)}
 				textColor={'black'}
 				onConfirm={onPickerConfirmHandler}
 				onCancel={onPickerCancelHandler}
