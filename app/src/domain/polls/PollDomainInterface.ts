3f0afa65b419dcf03283eb162955e167ec1a51f9
Wellington Souza <wellingtonsouza6300@gmail.com>
2024-04-02 13:04:37 -0400

    feat(poll): Implementa estrutura inicial de domínio de enquetes

---
+++ app/src/domain/polls/PollDomainInterface.ts
@@ -0,0 +1,5 @@
+interface PollDomainInterface {
+
+}
+
+export { PollDomainInterface }
---
+++ app/src/domain/polls/usePollDomain.ts
@@ -0,0 +1,16 @@
+import { PollDomainInterface } from './PollDomainInterface'
+
+function usePollDomain(): PollDomainInterface { // PollDomainInterface
+	return {
+		// getPollQuestions
+		// getPollResponses
+
+		// createNewPoll
+		// sendPollResponse
+		// registerUserOnPollResponseList
+
+		// deletePoll
+	}
+}
+
+export { usePollDomain }
