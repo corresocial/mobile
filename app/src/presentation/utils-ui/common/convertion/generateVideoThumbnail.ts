d5d7d50fcacbe84eae8be9f496121a6af6719886
Luis Eduardo <luiseddlvi@gmail.com>
2024-05-09 22:26:14 -0300

    feat(videoThumbnail): adiciona geração de thumbnail para os vídeos

--- app/src/presentation/utils-ui/common/convertion/generateVideoThumbnail.ts
+++ app/src/presentation/utils-ui/common/convertion/generateVideoThumbnail.ts
@@ -1,13 +1,13 @@
 import { createVideoThumbnail } from 'react-native-compressor'
 
 async function generateVideoThumbnails(videosUrl: string | string[]) {
-	if(videosUrl && typeof videosUrl === 'string'){
+	if (videosUrl && typeof videosUrl === 'string') {
 		const thumbnail = await createVideoThumbnail(videosUrl)
-		return  thumbnail.path
+		return thumbnail.path
 	}
 	
 	return Promise.all(
-		videosUrl.map(async (videoUrl) => {
+		(videosUrl as string[]).map(async (videoUrl: string) => {
 			const thumbnail = await createVideoThumbnail(videoUrl)
 			return thumbnail.path
 		})
