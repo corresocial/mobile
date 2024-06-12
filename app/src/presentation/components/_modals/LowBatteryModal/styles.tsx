888127e8ce5934a91c83f49a5bbbea1b8110724b
Luis Eduardo <luiseddlvi@gmail.com>
2024-06-12 20:55:26 -0300

    feat(LowBatteryModal): adiciona o modal de bateria baixa

---
+++ app/src/presentation/components/_modals/LowBatteryModal/styles.tsx
@@ -0,0 +1,53 @@
+import { RFValue } from 'react-native-responsive-fontsize'
+import styled from 'styled-components/native'
+
+import { relativeScreenHeight, relativeScreenWidth } from '@common/screenDimensions'
+
+export const ModalContainer = styled.View`
+    flex: 1;
+    justify-content: center;
+    padding-left: ${RFValue(15)}px;
+    padding-right: ${RFValue(15)}px;
+    background-color: ${({ theme }) => theme.transparence.red};
+`
+
+export const Container = styled.View`
+	height: ${relativeScreenHeight(27)}px;
+    border-radius: ${RFValue(23)}px;
+	margin-left: ${relativeScreenWidth(1.9)}px;
+    background-color: ${({ theme }) => theme.black4};
+`
+
+export const ContainerInner = styled.View`
+    flex: 1;
+	width: 100%;
+    height: 100%;
+	background-color: ${({ theme }) => theme.white3};
+    border: ${RFValue(6)}px solid ${({ theme }) => theme.black4};
+    border-radius: ${RFValue(23)}px;
+    position: absolute;
+	overflow: hidden;
+    align-items: center;
+    justify-content: space-around;
+    padding: ${RFValue(20)}px;
+	left: ${-relativeScreenWidth(2)}px;
+`
+
+export const CardTitleContainer = styled.View`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+`
+
+export const CardTitleText = styled.Text`
+    font-size: ${RFValue(20)}px;
+	font-family: Arvo_700Bold;
+	color: ${({ theme }) => theme.black4};
+    margin-left: ${RFValue(12)}px;
+`
+
+export const CardDescriptionText = styled.Text`
+    font-size: ${RFValue(12)}px;
+	font-family: Arvo_400Regular;
+	color: ${({ theme }) => theme.black4};
+`
