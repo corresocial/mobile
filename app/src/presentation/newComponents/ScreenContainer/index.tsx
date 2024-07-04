d9d57ae0f7d20f415dcb2f4e00f1f269451422b1
Luis Eduardo <luiseddlvi@gmail.com>
2024-07-04 17:53:17 -0300

    feat(ScreenContainer): adiciona a base do novo ScreenContainer

---
+++ app/src/presentation/newComponents/ScreenContainer/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { Container } from './styles'
+
+interface ScreenContainerProps{
+    color: string
+}
+
+function ScreenContainer({ color }: ScreenContainerProps) {
+	return (
+		<Container/>
+
+	)
+}
+
+export { ScreenContainer }
