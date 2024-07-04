2127f91e71ec678f394a0fd40f797d144c69f14c
Luis Eduardo <luiseddlvi@gmail.com>
2024-07-04 17:52:54 -0300

    feat(InfoDivider): adiciona o novo componente de divisor para o calendário

---
+++ app/src/presentation/newComponents/InfoDivider/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+import { ButtonArea, Container, DividerView, SubTitle, TextArea, Title } from './styles'
+
+interface InfoDividerProps{
+    title: string | number
+    subTitle?: string
+    onPress?: () => void
+}
+
+function InfoDivider({ title, subTitle, onPress }: InfoDividerProps) {
+	return (
+		<Container>
+			<DividerView>
+				<TextArea>
+					<Title>
+						{title}
+					</Title>
+					{
+						subTitle && (
+							<SubTitle>
+								{subTitle}
+							</SubTitle>
+						)
+					}
+				</TextArea>
+				<ButtonArea>
+					{/* Primary Button */}
+				</ButtonArea>
+			</DividerView>
+		</Container>
+
+	)
+}
+
+export { InfoDivider }
---
+++ app/src/presentation/newComponents/InfoDivider/styles.tsx
@@ -0,0 +1,40 @@
+import styled from 'styled-components/native'
+
+import { relativeScreenDensity } from '@common/screenDimensions'
+
+export const Container = styled.View`
+    width: 100%;
+    padding: 0 ${(relativeScreenDensity(10))}px;
+`
+
+export const DividerView = styled.View`
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    padding: ${(relativeScreenDensity(6))}px ${(relativeScreenDensity(20))}px;
+    border-radius: ${(relativeScreenDensity(16))}px;
+    width: 100%;
+    background-color: ${({ theme }) => theme.colors.white[3]};
+
+`   
+
+export const TextArea = styled.View`
+    flex-direction: column;
+    width: 50%;
+`
+
+export const ButtonArea = styled.View`
+    width: 50%;
+`
+
+export const Title = styled.Text`
+    font-family: Arvo_700Bold;
+    font-size: ${(relativeScreenDensity(18))}px;
+    color: ${({ theme }) => theme.colors.black[4]};
+`
+
+export const SubTitle = styled.Text`
+    font-family: Arvo_400Regular;
+    font-size: ${(relativeScreenDensity(12))}px;
+    color: ${({ theme }) => theme.colors.black[4]};
+`
