d4d184b7277df6267f9f1dfa405f57ba7e1a8f0c
Luis Eduardo <luiseddlvi@gmail.com>
2024-07-09 16:17:06 -0300

    feat(StandardButton): adiciona um novo botão standard

---
+++ app/src/presentation/newComponents/StandardButton/styles.ts
@@ -0,0 +1,58 @@
+import { relativeScreenDensity, relativeScreenWidth } from '@common/screenDimensions'
+import { ViewStyle } from 'react-native'
+import { RFValue } from 'react-native-responsive-fontsize'
+import styled from 'styled-components/native'
+
+
+interface ContainerBackgroundProps {
+	relativeWidth: string | number
+	relativeHeight: string | number
+	isIconButton: boolean
+}
+
+export const ContainerBackground = styled.View<ContainerBackgroundProps>`
+	flex-direction: row;
+	align-items: center;
+	justify-content: center;
+	margin-left: ${RFValue(6)}px;
+	position: relative;
+	background-color: ${({ theme }) => theme.black4};
+	height: ${({ relativeHeight }) => (typeof relativeHeight === 'string' ? relativeHeight : `${relativeScreenDensity(relativeHeight)}px`)};
+	width: ${(props) => (
+		props.isIconButton && typeof props.relativeHeight === 'number'
+			? `${relativeScreenDensity(props.relativeHeight)}px` : typeof props.relativeWidth === 'string'
+				? props.relativeWidth : `${relativeScreenDensity(props.relativeWidth)}px`
+	)};
+	border-radius: ${relativeScreenDensity(20)}px;
+`
+
+interface ContainerSurfaceProps {
+	backgroundColor: string
+	flexDirection: ViewStyle['flexDirection']
+	buttonPressed: boolean
+}
+
+export const ContainerSurface = styled.TouchableOpacity<ContainerSurfaceProps>`
+	height: 100%;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    border: ${RFValue(2.5)}px solid black;
+    position: absolute;
+	right: ${RFValue(5)}px;
+    flex-direction: ${({ flexDirection }) => flexDirection || 'row'};
+	transform: ${({ buttonPressed }) => (buttonPressed ? 'translateX(5px)' : 'translateX(0px)')};
+	background-color: ${({ theme, backgroundColor }) => backgroundColor || theme.white3};
+	border-radius: ${relativeScreenDensity(20)}px;
+	gap: ${relativeScreenDensity(10)}px;
+`
+
+interface ButtonTextProps{
+    textTheme: 'light' | 'dark'
+}
+
+export const ButtonText = styled.Text<ButtonTextProps>`
+    font-family: ${({ theme }) => theme.fonts.arvoRegular};
+    font-size: ${({ theme }) => theme.fontSizes.arvo[3]}px;
+    color: ${({ textTheme, theme }) => (textTheme === 'light' ? theme.colors.white[3] : theme.colors.black[4])};
+`
