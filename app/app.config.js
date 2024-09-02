4e3779d65adbed108457ba93a106a6e711bf99c2
Wellington Souza Abreu <64331839+WellingtonSouzaAbreu@users.noreply.github.com>
2024-09-02 14:08:52 -0400

    chore(version): Atualiza targed api android

--- app/app.config.js
+++ app/app.config.js
@@ -39,8 +39,8 @@ export default {
 						useFrameworks: 'static'
 					},
 					android: {
-						compileSdkVersion: 33,
-						targetSdkVersion: 33,
+						compileSdkVersion: 34,
+						targetSdkVersion: 34,
 						buildToolsVersion: '31.0.0',
 						enableProguardInReleaseBuilds: true,
 						extraProguardRules: '-keep public class com.horcrux.svg.** {*;}',
